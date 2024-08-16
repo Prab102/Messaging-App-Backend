131ca8a1376b55ac037e7f4f4f0f456e8335c22f
Prab Singh <prabjot246@gmail.com>
2024-08-15 23:41:10 -0400

    added domain to cookie

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(passport.initialize());
-app.use(cors({ credentials: true, origin:"http://localhost:5173", exposedHeaders: ["set-cookie"] })) //change when live or testing
+app.use(cors({ credentials: true, origin:"*", exposedHeaders: ["set-cookie"],})) //change when live or testing
 
 // app.use(cors({ credentials: true, origin:"https://messaging-app-frontend-seven.vercel.app", exposedHeaders: ["set-cookie"] })) //change when live or testing
 
